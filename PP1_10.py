7482825488b86ff921b8078a31eefd6411423fa7
Khirushan-Sasitharan-ICD2O1-01 <khirushan.sasitharan@student.tdsb.on.ca>
2024-10-02 13:48:21 +0000

    done

--- PP1_10.py
+++ PP1_10.py
@@ -1,31 +1,39 @@
-
+import math
 
 def q1(): 
   #Write Assignment code here
-
+  anson=eval(input("Input a number: "))
+  print(math.sqrt(anson))
 
 def q2(): 
   #Write Assignment code here
-
-
+  skub=eval(input("Input a number: "))
+  print(math.isqrt(skub))
 def q3(): 
   #Write Assignment code here
-
-
+  ddhdh=eval(input("Input a number: "))
+  print(math.floor(ddhdh))
 def q4(): 
   #Write Assignment code here
+  rehehe=eval(input("Input a number: "))
+  print(math.ceil(rehehe))
 
 
 def q5(): 
   #Write Assignment code here
-
+  skib1=eval(input("Input a number: "))
+  skib2=eval(input("Input another number: "))
+  skib3=skib1*skib2
+  skib4=skib3//2
+  skib5=math.floor((skib4))
+  print(skib5)
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
